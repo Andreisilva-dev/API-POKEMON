2b849e1dcfa683d07b6575a1141946b0944133e6
Andreisilva-dev <andrei765silva@gmail.com>
2025-07-09 13:53:09 -0300

    formatacao corrigida

--- js/scrip.js
+++ js/scrip.js
@@ -1,45 +1,38 @@
+let conteudo = document.querySelector(".conteudo");
 
-        let conteudo = document.querySelector('.conteudo')
+function chama() {
+  let i = 0;
+  while (i <= 151) {
+    console.log(i);
+    fetch(`https://pokeapi.co/api/v2/pokemon/${i}`)
+      .then((response) => response.json())
+      .then((dados) => {
+        console.log(dados);
 
-        function chama(){
+        // criando meu card
+        let card = document.createElement("div");
+        // colocando minha div no meu html conteudo
+        conteudo.appendChild(card);
+        card.classList.add("card");
 
+        // colocando img no card
+        let img = document.createElement("img");
+        card.appendChild(img);
+        img.src = dados.sprites.front_default;
 
-            let i = 0
-            while(i <= 151){
-                console.log(i)
-                fetch(`https://pokeapi.co/api/v2/pokemon/${i}`)
-                    .then(response => response.json())
-                    .then(dados => {
-                    console.log(dados)
+        // colocando nome pokemon
+        let namePokemon = document.createElement("p");
+        card.appendChild(namePokemon);
+        namePokemon.textContent = dados.name;
 
-                
-                    // criando meu card
-                    let card = document.createElement('div')
-                    // colocando minha div no meu html conteudo
-                    conteudo.appendChild(card)
-                    card.classList.add('card')
-                    
-                    // colocando img no card
-                    let img = document.createElement('img')
-                    card.appendChild(img)
-                    img.src = dados.sprites.front_default
+        // criar tipo
+        let tipoPokemon = document.createElement("p");
+        card.appendChild(tipoPokemon);
+        tipoPokemon.textContent = dados.types[0].type.name;
+        tipoPokemon.classList.add("botaoTipo");
+      });
+    i++;
+  }
 
-                    // colocando nome pokemon
-                    let namePokemon = document.createElement('p')
-                    card.appendChild(namePokemon)
-                    namePokemon.textContent = dados.name
-
-                    // criar tipo
-                    let tipoPokemon = document.createElement('p')
-                    card.appendChild(tipoPokemon)
-                    tipoPokemon.textContent = dados.types[0].type.name
-                    tipoPokemon.classList.add('botaoTipo')
-                    
-                })
-                i++
-            }
-
-            conteudo.classList.add('conteudo')
-
-
-        }
+  conteudo.classList.add("conteudo");
+}
